ea63b762425f3cb5887e99b99b4ff3467dd0b3e7
matthias <mf@sapstern.com>
2024-03-15 13:58:10 +0100

    2024_03_15

---
+++ app/build/generated/source/buildConfig/debug/org/mfri/bbc/mediamanager/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.mfri.bbc.mediamanager;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.mfri.bbc.mediamanager";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 100;
+  public static final String VERSION_NAME = "v1.0.0";
+}
